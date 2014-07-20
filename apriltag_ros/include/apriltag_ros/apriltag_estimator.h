e70c137f719a4d20fe0c4ccb952539f418eae720
Chao Qu <quchao@seas.upenn.edu>
2014-07-20 01:08:23 -0400

    move usb_camera to another package

---
+++ apriltag_ros/include/apriltag_ros/apriltag_estimator.h
@@ -0,0 +1,10 @@
+#ifndef APRILTAG_ESTIMATOR_H
+#define APRILTAG_ESTIMATOR_H
+
+class ApriltagEstimator
+{
+public:
+  ApriltagEstimator();
+};
+
+#endif // APRILTAG_ESTIMATOR_H
---
+++ apriltag_ros/src/apriltag_estimator.cpp
@@ -0,0 +1,5 @@
+#include "apriltag_estimator.h"
+
+ApriltagEstimator::ApriltagEstimator()
+{
+}
